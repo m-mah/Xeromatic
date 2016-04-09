e2b561c2cc7d63c94dbb671e89e27a9727eb4dcb
Emma <Emma>
2016-04-09 12:55:05 +1200

    Added a tweet interface, used gettweets from api to display recent tweets

--- Project/Xeromatic/Services/TwitterApiService.cs
+++ Project/Xeromatic/Services/TwitterApiService.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Tweetinvi;
 using Tweetinvi.Core.Credentials;
-using Tweetinvi.Core.Interfaces;
+using Tweet = Xeromatic.Models.Tweet;
+
 
 namespace Xeromatic.Services
 {
@@ -16,21 +18,25 @@ namespace Xeromatic.Services
         {
             _creds = new TwitterCredentials
             {
-                ConsumerKey = "Add your ConsumerKey here",
-                ConsumerSecret = "Add your ConsumerSecret here",
-                AccessToken = "Add your AccessToken here",
-                AccessTokenSecret = "Add your AccessTokenSecret here"
+                ConsumerKey = "4c0hybMEURve3dgLwJKlNtJkL",
+                ConsumerSecret = "6ZvdxojCLjJS8tolObYKlixgfaIclOXSAeqptWQCacWnCnvbKY",
+                AccessToken = "355836672-OUuzbTuW27WWEYSp375X6ZIqa5PQUNXwnCBTe9nI",
+                AccessTokenSecret = "KobVQS2KffduwhQdLirxnOYejX4IErY4nA2MZEKIJm8o3"
             };
         }
 
-        public IEnumerable<ITweet> GetTweets()
+        public IEnumerable<Tweet> GetTweets()
         {
-            var tweets = Auth.ExecuteOperationWithCredentials(_creds, () =>
-            {
-                return Timeline.GetHomeTimeline();
-            });
+            var tweets = Auth.ExecuteOperationWithCredentials(_creds, () => Timeline.GetUserTimeline("xero", 10))?.ToList();
 
-            return tweets;
+            if (tweets != null && tweets.Any())
+                return tweets.Select(t => new Tweet()
+                {
+                    Id = t.Id,
+                    Text = t.Text
+                });
+
+            return new List<Tweet>();
         }
 
     }
---
+++ Project/Xeromatic/Services/iTwitterService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Xeromatic.Models;
+
+namespace Xeromatic.Services
+{
+    public interface iTwitterService
+    {
+        IEnumerable<Tweet> GetTweets();
+    }
+}
